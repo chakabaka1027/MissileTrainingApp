cc3113fcafc875431aa617dd1a39b05d155495ca
chakabaka1027 <alexanderdcha@gmail.com>
2017-12-07 11:25:56 -0500

    collaboration tool functioning, (multiplayer)

--- Assets/Scripts/FaceCamera.cs
+++ Assets/Scripts/FaceCamera.cs
@@ -11,6 +11,8 @@ public class FaceCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt (transform.position + Camera.main.transform.rotation * Vector3.forward/*, Camera.main.transform.rotation * Vector3.up*/);
+        if(Camera.main != null){
+            transform.LookAt (transform.position + Camera.main.transform.rotation * Vector3.forward/*, Camera.main.transform.rotation * Vector3.up*/);
+        }
 	}
 }
---
+++ Assets/Scripts/SetupLocalPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Vuforia;
+
+public class SetupLocalPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer){
+            //GetComponent<VuforiaBehaviour>().enabled = true;
+            //this.gameObject.SetActive(true);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
